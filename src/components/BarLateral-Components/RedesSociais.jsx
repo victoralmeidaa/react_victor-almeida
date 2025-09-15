feb4c9c14c7db0fc56dcc5a8a0a0374bfd9c0854
Victor <victordealmeida5@gmail.com>
2025-09-15 14:58:02 -0300

    add icons barLateral

--- src/components/BarLateral-Components/RedesSociais.jsx
+++ src/components/BarLateral-Components/RedesSociais.jsx
@@ -1,29 +1,19 @@
+import { FaLinkedinIn, FaGithub, FaInstagram } from "react-icons/fa";
+
 
 export default function RedesSociais(){
     return (
-        <div className="perfil-redes">
-        <a
-          href="https://www.linkedin.com/in/seu-usuario"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src="/logos/linkedin.png" alt="LinkedIn" className="perfil-icone"/>
+      <div className="perfil-redes">
+        <a href="https://www.linkedin.com/in/victalmeida/" target="_blank" rel="noopener noreferrer">
+          <FaLinkedinIn className="perfil-icone" />
         </a>
 
-        <a
-          href="https://github.com/victoralmeidaa"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src="/logos/github.png" alt="GitHub" className="perfil-icone" />
+        <a href="https://github.com/victoralmeidaa" target="_blank" rel="noopener noreferrer">
+          <FaGithub className="perfil-icone" />
         </a>
 
-        <a
-          href="https://www.instagram.com/seu-usuario"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src="/logos/instagram.png" alt="Instagram" className="perfil-icone"/>
+        <a href="https://www.instagram.com/seu-usuario" target="_blank" rel="noopener noreferrer">        
+          <FaInstagram className="perfil-icone" />
         </a>
       </div>
     )
