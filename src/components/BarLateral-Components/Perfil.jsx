d3a714a11a64c567c648d7dc6d409c04d99a10ad
Victor <victordealmeida5@gmail.com>
2025-09-15 12:27:37 -0300

    add components redesSociais

---
+++ src/components/BarLateral-Components/Perfil.jsx
@@ -0,0 +1,17 @@
+//import { AiFillPhone, AiOutlineMail, AiFillEnvironment } from "react-icons/ai"
+import "../../styles/Perfil.css";
+import MinhaFoto from "../../img/eu.jpg";
+import RedesSociais from "./RedesSociais.jsx";
+
+export default function Perfil() {
+  return (
+    <div className="perfil">
+        <img src={MinhaFoto} alt="Foto de perfil" className="perfil-foto" />
+        <h2 className="perfil-name">Victor Almeida</h2>
+        <p className="perfil-descricao">Desenvolvedor Fullstack em Construçao</p>
+        <div className="redesSociais">
+            <RedesSociais />
+        </div>
+    </div>
+  );
+}
---
+++ src/components/BarLateral-Components/RedesSociais.jsx
@@ -0,0 +1,30 @@
+
+export default function RedesSociais(){
+    return (
+        <div className="perfil-redes">
+        <a
+          href="https://www.linkedin.com/in/seu-usuario"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src="/logos/linkedin.png" alt="LinkedIn" className="perfil-icone"/>
+        </a>
+
+        <a
+          href="https://github.com/victoralmeidaa"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src="/logos/github.png" alt="GitHub" className="perfil-icone" />
+        </a>
+
+        <a
+          href="https://www.instagram.com/seu-usuario"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src="/logos/instagram.png" alt="Instagram" className="perfil-icone"/>
+        </a>
+      </div>
+    )
+}
